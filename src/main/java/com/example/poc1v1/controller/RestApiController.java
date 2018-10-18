eaa29fb8625f0253439342dc8df21089dd126ed7
Steven Shaginyan <steven.shaginyan@heroku.com>
2018-10-18 16:21:28 -0700

    Testing

--- src/main/java/com/example/poc1v1/controller/RestApiController.java
+++ src/main/java/com/example/poc1v1/controller/RestApiController.java
@@ -1,18 +1,23 @@
 package com.example.poc1v1.controller;
 
+import com.google.gson.Gson;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.poc1v1.model.Data;
+
 @RestController
 @RequestMapping("/api")
 public class RestApiController {
 
     @PostMapping("/send-message")
-    public ResponseEntity<Void> sendMessage(){
-        System.out.println("Testing123");
+    public ResponseEntity<Void> sendMessage(@RequestBody Data data) {
+        Gson gson = new Gson();
+        String jsonString = gson.toJson(data);
+        System.out.println(jsonString);
         return null;
     }
 
